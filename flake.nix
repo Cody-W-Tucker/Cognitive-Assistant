b959aa9e71f7c41be0f54099a6ecbaaf9e0bb25a
Cody Tucker <cody@tmvsocial.com>
2025-09-04 10:09:20 -0500

    only install requirements if venv doesn't exist

--- flake.nix
+++ flake.nix
@@ -29,9 +29,13 @@
               scikit-learn
             ]);
           shellHook = ''
-            python -m venv .venv
-            source .venv/bin/activate
-            pip install -r requirements.txt
+            if [ ! -d ".venv" ]; then
+              python -m venv .venv
+              source .venv/bin/activate
+              pip install -r requirements.txt
+            else
+              source .venv/bin/activate
+            fi
           '';
         };
       });
